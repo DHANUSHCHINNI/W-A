eb3a688a0daf10244fede4fa0c5b6c91328db30f
Dhanush <dhanushchinni100@gmail.com>
2025-08-15 22:54:17 +0530

    training hub magrin fixed

--- src/app/traininghub/trainingText.tsx
+++ src/app/traininghub/trainingText.tsx
@@ -5,14 +5,14 @@ export const trainingTextsection1 = [
 ];
 
 export const trainingTextsection1b = [
-    <div key="2" style={{ fontFamily: 'PT Serif, serif', fontSize: '1.7rem', color: '#BAB1AB' }}>
+    <div key="2" style={{ fontFamily: 'PT Serif, serif', fontSize: '1.7rem', color: '#BAB1AB', marginTop: '5.2rem' }}>
         We specialize in art-based learning, somatic education, and mental health workshops for students, educators, therapists, and organizations. Our programs combine the latest in neuroscience, creative practices, and emotional support to help individuals thrive in academic, professional, and community settings.<br />
         We collaborate with schools, colleges, NGOs, and corporate teams across India and beyond—offering both in-person and online formats.
     </div>
 ];
 
 export const trainingTextsection2 = [
-    <div key="2" style={{ fontFamily: 'PT Serif, serif', fontSize: '1.7rem', color: '#BAB1AB' }}>
+    <div key="2" style={{ fontFamily: 'PT Serif, serif', fontSize: '1.7rem', color: '#BAB1AB', marginTop: '5.2rem' }}>
         Looking to bring arts-based mental health workshops, creative facilitation training, or student well-being programs to your space?<br />
         Explore how the Training Hub can support your learning journey.
     </div>
